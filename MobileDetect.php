8644f4df384638c841a18fde9b6fa6ec1eee6eaf
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-07-20 14:43:17 -0600

    Update MobileDetect.php

--- MobileDetect.php
+++ MobileDetect.php
@@ -12,5 +12,5 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 
 	return;
 } else {
-	die( 'This version of the MobileDetect extension requires MediaWiki 1.29+' );
+	die( 'This version of the MobileDetect extension requires MediaWiki 1.35+' );
 }
